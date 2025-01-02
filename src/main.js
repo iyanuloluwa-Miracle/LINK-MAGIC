7696d9934c6232e2fbd63ed9a2af7cefb259eb9a
iyanuloluwa-Miracle <iyanudina@gmail.com>
2025-01-02 10:18:24 +0100

    feat: initialize google analytics in the mainjs file

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "./router";
+import VueGtag from "vue-gtag";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(
+  VueGtag,
+  {
+    config: { id: "G-XXXXXXXXXX" }, // Replace with your GA4 
+  },
+  router
+);
+
+app.use(router);
+app.mount("#app");
