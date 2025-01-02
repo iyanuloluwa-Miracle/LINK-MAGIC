0fc4bfee62efe90902f323c8cfa889788eb64210
iyanuloluwa-Miracle <iyanudina@gmail.com>
2025-01-02 10:49:27 +0100

    fixes: fixed deployment link that is failing

--- src/main.js
+++ src/main.js
@@ -1,18 +1,12 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import router from "./router";
 import VueGtag from "vue-gtag";
 
 const app = createApp(App);
 
-app.use(
-  VueGtag,
-  {
-    config: { id: process.env.VUE_APP_GA_ID }, 
-  },
-  router
-);
+app.use(VueGtag, {
+  config: { id: import.meta.env.VITE_GA_ID }, // Replace with your GA4
+});
 
-app.use(router);
 app.mount("#app");
